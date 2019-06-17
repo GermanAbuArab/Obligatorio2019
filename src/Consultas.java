9b40d90acc417c198cf8e3340305440f768a1dff
Germán Abu Arab <gabuarab@correo.um.edu.uy>
2019-06-17 13:20:43 -0300

    push de la salvacion 3.0

--- src/Consultas.java
+++ src/Consultas.java
@@ -1,16 +1,7 @@
 import entidades.*;
-import tads.hash.Hash;
 import tads.hash.NodoHash;
-import tads.heap.Heap;
 import tads.heap.HeapImpl;
-import tads.heap.NodeHeap;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Consultas {
 
@@ -19,38 +10,53 @@ public class Consultas {
         NodoHash<Athlete, Integer>[] temp = Repositorio.getHashAtleta().getHash();
         if (tipo == 1) {
 
-            HeapImpl<Integer, Athlete> medallasOro =new HeapImpl<>(150000,1) ;
+            HeapImpl<Integer, Athlete> medallasOro = new HeapImpl<>(150000, 1);
             for (int i = 0; i < temp.length; i++) {
                 if (temp[i] != null) {
                     medallasOro.agregar(temp[i].getValor().getMedallasOro(), temp[i].getValor());
                 }
             }
+            for (int i = 0; i < 10; i++) {
+                Athlete athlete = medallasOro.obtenerYEliminar().getData();
+                System.out.println("Nombre: " + athlete.getName() + " - Sexo: " + athlete.getSex() + " - Medallas oro: "
+                        + athlete.getMedallasOro() + " - Año minimo: " + athlete.getAnioMinimo() + " - Año maximo: " + athlete.getAnioMaximo()); //agregar vector de 2 ´para conseguir año max y minimo en la clase atleta
+            }
         } else if (tipo == 2) {
-            HeapImpl<Integer, Athlete> medallasPlata =new HeapImpl<>(150000,1) ;
+            HeapImpl<Integer, Athlete> medallasPlata = new HeapImpl<>(150000, 1);
             for (int i = 0; i < temp.length; i++) {
                 if (temp[i] != null) {
                     medallasPlata.agregar(temp[i].getValor().getMedallasPlata(), temp[i].getValor());
                 }
             }
-        }else if (tipo == 3) {
-            HeapImpl<Integer, Athlete> medallasBronce =new HeapImpl<>(150000,1) ;
+            for (int i = 0; i < 10; i++) {
+                Athlete athlete = medallasPlata.obtenerYEliminar().getData();
+                System.out.println("Nombre: " + athlete.getName() + " - Sexo: " + athlete.getSex() + " - Medallas plata: "
+                        + athlete.getMedallasPlata() + " - Año minimo: " + athlete.getAnioMinimo() + " - Año maximo: " + athlete.getAnioMaximo()); //agregar vector de 2 ´para conseguir año max y minimo en la clase atleta
+            }
+
+        } else if (tipo == 3) {
+            HeapImpl<Integer, Athlete> medallasBronce = new HeapImpl<>(150000, 1);
             for (int i = 0; i < temp.length; i++) {
                 if (temp[i] != null) {
                     medallasBronce.agregar(temp[i].getValor().getMedallasBronce(), temp[i].getValor());
                 }
             }
-        }
-        else if (tipo == 4) {
-            HeapImpl<Integer, Athlete> medallas =new HeapImpl<>(150000,1) ;
-            for (int i = 0; i < temp.length-1; i++) {
-
+            for (int i = 0; i < 10; i++) {
+                Athlete athlete = medallasBronce.obtenerYEliminar().getData();
+                System.out.println("Nombre: " + athlete.getName() + " - Sexo: " + athlete.getSex() + " - Medallas bronce: "
+                        + athlete.getMedallasBronce() + " - Año minimo: " + athlete.getAnioMinimo() + " - Año maximo: " + athlete.getAnioMaximo()); //agregar vector de 2 ´para conseguir año max y minimo en la clase atleta
+            }
+        } else if (tipo == 4) {
+            HeapImpl<Integer, Athlete> medallas = new HeapImpl<>(150000, 1);
+            for (int i = 0; i < temp.length; i++) {
                 if (temp[i] != null) {
                     medallas.agregar(temp[i].getValor().getMedallas(), temp[i].getValor());
                 }
             }
-            for(int i=0;i<10;i++){
-                Athlete athlete=medallas.obtenerYEliminar().getData();
-                System.out.println("Nombre: "+ athlete.getName()+ " - Sexo: "+ athlete.getSex()+" - Medallas: "+ athlete.getMedallas()+" - Año minimo: "+ athlete.getAnioMinimo()+" - Año maximo: "+ athlete.getAnioMaximo()); //agregar vector de 2 ´para conseguir año max y minimo en la clase atleta
+            for (int i = 0; i < 10; i++) {
+                Athlete athlete = medallas.obtenerYEliminar().getData();
+                System.out.println("Nombre: " + athlete.getName() + " - Sexo: " + athlete.getSex() + " - Medallas: "
+                        + athlete.getMedallas() + " - Año minimo: " + athlete.getAnioMinimo() + " - Año maximo: " + athlete.getAnioMaximo()); //agregar vector de 2 ´para conseguir año max y minimo en la clase atleta
             }
         }
 
--- src/Menu.java
+++ src/Menu.java
@@ -4,35 +4,65 @@ public class Menu {
 
     public static void main(String args[]) {
 
-    CargaDeDatos.carga();
-    String choice = null;
-    Scanner sc =new Scanner(System.in);
+        CargaDeDatos.carga();
+        String choice = null;
+        Scanner sc = new Scanner(System.in);
 
-    do{
-        System.out.println("-------------[Menu Principal]--------------");
-        System.out.println("1 - Indicar el top 10 de atletas que consiguieron la mayor cantidad de medallas en la historia de los juegos");
-        System.out.println("2 - Indicar el top 10 de regiones que consiguieron la mayor cantidad de medallas en la historia de los juegos" );
-        System.out.println("3 - Indicar el top 10 de ediciones de los juegos olímpicos donde se tuvo mayor participación de atletas femeninos");
-        System.out.println("4 - Indicar las 5 competiciones donde se presentan la mayor cantidad de atletas de cierto sexo. Si es femenino o masculino se debe solicitar como dato de entrada");
-        System.out.println("5 - Indicar los 5 equipos mas efectivos entre un rango de años (especificadas al ejecutar el reporte)");
-        System.out.println("0 - Terminar");
+        do {
+            System.out.println("-------------[Menu Principal]--------------");
+            System.out.println("1 - Indicar el top 10 de atletas que consiguieron la mayor cantidad de medallas en la historia de los juegos");
+            System.out.println("2 - Indicar el top 10 de regiones que consiguieron la mayor cantidad de medallas en la historia de los juegos");
+            System.out.println("3 - Indicar el top 10 de ediciones de los juegos olímpicos donde se tuvo mayor participación de atletas femeninos");
+            System.out.println("4 - Indicar las 5 competiciones donde se presentan la mayor cantidad de atletas de cierto sexo. Si es femenino o masculino se debe solicitar como dato de entrada");
+            System.out.println("5 - Indicar los 5 equipos mas efectivos entre un rango de años (especificadas al ejecutar el reporte)");
+            System.out.println("0 - Terminar");
 
-        choice=sc.nextLine();
-        switch (choice){
-            case "1":
-                    Consultas.consultaUno(4);
-                break;
-            case "2":
+            choice = sc.nextLine();
+            switch (choice) {
+                case "1":
+                    String choice2 = null;
+                    do {
+                        System.out.println("1 - Por medallas de oro");
+                        System.out.println("2 - Por medallas de plata");
+                        System.out.println("3 - Por medallas de bronce");
+                        System.out.println("4 - Por medallas en total");
+                        System.out.println("0 - Volver al Menu");
+                        choice2 = sc.nextLine();
+                        if (!choice2.equals("0") && !choice2.equals("1") && !choice2.equals("2") && !choice2.equals("3") && !choice2.equals("4")) {
+                            System.out.println("Operacion no valida");
+                        }
 
-                break;
-            case "3":
+                        switch (choice2) {
+                            case "1":
+                                Consultas.consultaUno(1);
+                                choice2 = "0";
+                                break;
+                            case "2":
+                                Consultas.consultaUno(2);
+                                choice2 = "0";
+                                break;
+                            case "3":
+                                Consultas.consultaUno(3);
+                                choice2 = "0";
+                                break;
+                            case "4":
+                                Consultas.consultaUno(4);
+                                choice2 = "0";
+                                break;
+                        }
+                    } while (!choice2.equals("0"));
+                    break;
+                case "2":
 
-                break;
-            case "4":
+                    break;
+                case "3":
 
-                break;
+                    break;
+                case "4":
+
+                    break;
+            }
         }
-    }
         while (!choice.equals("0"));
         sc.close();
 
