c8a06e64d7b28a58eb92d9f9ca8adec79e5793d3
Matias <hola>
2019-06-18 22:55:00 -0300

    retoques repo

--- src/Repositorio.java
+++ src/Repositorio.java
@@ -44,9 +44,6 @@ public class Repositorio {
         return hashGames;
     }
 
-    public static ArrayList<AthleteOlympicParticipation> getListaParticip() {
-        return listaParticip;
-    }
 
     public static Hash<Event, String> getHashEventos() {
         return hashEventos;
@@ -56,72 +53,5 @@ public class Repositorio {
         Repositorio.hashEventos = hashEventos;
     }
 
-    //public static void setHashParticip(Hash<AthleteOlympicParticipation, Integer> hashParticip) {
-      //  Repositorio.hashParticip = hashParticip;
-    //}
-
-//    public static Heap<Integer, Athlete> getMedallasOro() {
-//        return medallasOro;
-//    }
-//
-//    public static void setMedallasOro(Heap<Integer, Athlete> medallasOro) {
-//        medallasOro = medallasOro;
-//    }
-//
-//    public static Heap<Integer, Athlete> getMedallasPlata() {
-//        return medallasPlata;
-//    }
-//
-//    public static void setMedallasPlata(Heap<Integer, Athlete> medallasPlata) {
-//        medallasPlata = medallasPlata;
-//    }
-//
-//    public static Heap<Integer, Athlete> getMedallasBronce() {
-//        return medallasBronce;
-//    }
-//
-//    public static void setMedallasBronce(Heap<Integer, Athlete> medallasBronce) {
-//        medallasBronce = medallasBronce;
-//    }
-//
-//    public static Heap<Integer, Athlete> getMedallas() {
-//        return medallas;
-//    }
-//
-//    public static void setMedallas(Heap<Integer, Athlete> medallas) {
-//        medallas = medallas;
-//    }
-//
-//    public static Heap<Integer, RegionPorMedalla> getMedallasOroReg() {
-//        return medallasOroReg;
-//    }
-//
-//    public static void setMedallasOroReg(Heap<Integer, RegionPorMedalla> medallasOroReg) {
-//        medallasOroReg = medallasOroReg;
-//    }
-//
-//    public static Heap<Integer, RegionPorMedalla> getMedallasPlataReg() {
-//        return medallasPlataReg;
-//    }
-//
-//    public static void setMedallasPlataReg(Heap<Integer, RegionPorMedalla> medallasPlataReg) {
-//        medallasPlataReg = medallasPlataReg;
-//    }
-//
-//    public static Heap<Integer, RegionPorMedalla> getMedallasBronceReg() {
-//        return medallasBronceReg;
-//    }
-//
-//    public static void setMedallasBronceReg(Heap<Integer, RegionPorMedalla> medallasBronceReg) {
-//        medallasBronceReg = medallasBronceReg;
-//    }
-//
-//    public static Heap<Integer, RegionPorMedalla> getMedallasReg() {
-//        return medallasReg;
-//    }
-
-//    public static void setMedallasReg(Heap<Integer, RegionPorMedalla> medallasReg) {
-//        medallasReg = medallasReg;
-//    }
 
 }
